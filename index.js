b0b93b21035649240472e41e728f9129e4f2e26a
Hoxtygen <hoxtygen@live.com>
2019-07-08 13:33:27 +0100

    Get a single user
    Given a valid id, a single user can be obtained. However, if an invalid id is supplid a 404 is thrown to show that the user does not exist.

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    UsersModel.findById(id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    status: 404,
+                    message: 'User not found'
+                })
+            }
+            return res.status(200).json({
+                status: 200,
+                user
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                message: err
+            })
+        })
+})
+
 server.listen(5000, () => {
     console.log('listening on 5000');
 
