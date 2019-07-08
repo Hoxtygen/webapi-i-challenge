9c0d7a2155daedeb44acb8e45c9e146e43af3e1f
Hoxtygen <hoxtygen@live.com>
2019-07-08 13:21:32 +0100

    Get all users
    An endpoint to get all users have been added, there are two users at this point.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // implement your API here
+const express = require('express');
+const server = express();
+
+const UsersModel = require('./data/db.js');
+
+server.use(express.json())
+server.get('/', (req, res) => {
+    res.send('Hello, this is the lambda users api')
+})
+
+server.get('/api/users', (req, res) => {
+    UsersModel.find()
+        .then(users => {
+            res.status(200).json({
+                status: 200,
+                users
+            })
+        })
+})
+
+server.listen(5000, () => {
+    console.log('listening on 5000');
+
+  });
