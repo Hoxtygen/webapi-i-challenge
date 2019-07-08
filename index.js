fd014200cfbd703b5f01bea6c3dde5ef09a4574c
Hoxtygen <hoxtygen@live.com>
2019-07-08 14:03:17 +0100

    Add a new user
    Endpoint for adding a new user was added, including error-checking the added information to make sure all needed infos are included.

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ server.get('/api/users', (req, res) => {
                 users
             })
         })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                error: "The users information could not be retrieved."
+            })
+        })
 })
 
 server.get('/api/users/:id', (req, res) => {
@@ -26,7 +32,7 @@ server.get('/api/users/:id', (req, res) => {
             if (!user) {
                 return res.status(404).json({
                     status: 404,
-                    message: 'User not found'
+                    message: "The user with the specified ID does not exist."
                 })
             }
             return res.status(200).json({
@@ -37,8 +43,26 @@ server.get('/api/users/:id', (req, res) => {
         .catch(err => {
             res.status(500).json({
                 status: 500,
-                message: err
+                error: "The user information could not be retrieved."
             })
+        })
+});
+
+server.post('/api/users', (req, res) => {
+    let { name, bio} = req.body;
+    const newUser = { name, bio }
+    if (!name || !bio) {
+        return res.status(400).json({
+            status: 400,
+            errorMessage: "Please provide name and bio for the user." 
+        })
+    }
+    UsersModel.insert(newUser)
+        .then(user => {
+           return res.status(201).json({
+               status: 200,
+               user
+           })
         })
 })
 
